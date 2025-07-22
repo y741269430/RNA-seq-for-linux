dd38adc3d34551c663e2dc7b6e35db3be95e204a
JJYang <56186383+y741269430@users.noreply.github.com>
2025-07-22 20:51:04 +0800

    Delete code directory

--- code/rna4_rmcounts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-## remove redundant rows ##
-
-tail -n 5 ./rawcounts/* > ./rawcounts/total.info
-
-cat filenames | while read i; 
-do
-sed -i '/process/d;/__/d;/retrieve/d' ./rawcounts/${i}.count & 
-done
